ff4109173878356190fbb5da59aed515c1cf3748
Sam Ng <linksofrich.mail.sam@gmail.com>
2023-11-17 15:03:08 +0800

    Initial Commit

---
+++ FMWHK/FMWHKApp.swift
@@ -0,0 +1,17 @@
+//
+//  FMWHKApp.swift
+//  FMWHK
+//
+//  Created by Sam Ng on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FMWHKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
