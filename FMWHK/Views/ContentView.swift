3ff8a77aa9b72f3ae1738cf71fc653370a3ababd
Sam Ng <linksofrich.mail.sam@gmail.com>
2023-11-20 16:17:53 +0800

    Added navigation drawer

--- FMWHK/Views/ContentView.swift
+++ FMWHK/Views/ContentView.swift
@@ -4,113 +4,101 @@
 //
 //  Created by Sam Ng on 17/11/2023.
 //
-
 import SwiftUI
-import Foundation
-import CoreLocation
-import Combine
 
 struct ContentView: View {
-    @State var stopList: [Stop]?
-    @State var nearestStopList: [Stop]?
-    @StateObject var locationManager = LocationManager()
-    var userLocation: CLLocation {
-        return CLLocation(latitude: locationManager.lastLocation?.coordinate.latitude ?? curLocation.coordinate.latitude, longitude: locationManager.lastLocation?.coordinate.longitude ?? curLocation.coordinate.longitude)
-    }
-    var userLatitude: String {
-            return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
-        }
-        
-    var userLongitude: String {
-        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
-    }
-
+    @State var isDrawerOpen: Bool = false
+    @State var selectedView: String = "Home"
     
     var body: some View {
-        ScrollView {
-            VStack{
-                ForEach(nearestStopList ?? [], id: \.stop) { stop in
-                    NavigationLink(destination: StopView(stop: stop)) {
-                        VStack {
-                            StopRowView(stop: stop)
-                                .frame(width: UIScreen.screenWidth, height: 100)
+        ZStack {
+            // Main View based on selectedView
+            Group {
+                switch selectedView {
+                case "Home":
+                    Text("Home Screen")
+                case "Screen One":
+                    StopListView()
+                case "Screen Two":
+                    Text("Screen Two")
+                default:
+                    Text("Home Screen")
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white)
+            .disabled(isDrawerOpen)
+            
+            // Drawer Button
+            VStack {
+                HStack {
+                    Button(action: {
+                        withAnimation {
+                            self.isDrawerOpen.toggle()
                         }
+                    }) {
+                        Image(systemName: "line.horizontal.3")
+                            .imageScale(.large)
+                            .padding()
                     }
+                    Spacer()
                 }
+                Spacer()
             }
-            .task{
-                do {
-                    stopList = try await getStopData()
-                    nearestStopList = getNearestStops(stopList ?? [], k: 10, srcCoordinate: userLocation)
-                    print("\(userLatitude)")
-                    print("\(userLongitude)")
-                } catch NetworkError.invalidURL {
-                    print("invalid URL")
-                } catch NetworkError.invalidResponse {
-                    print("invalid response")
-                } catch NetworkError.invalidData {
-                    print("invalid data")
-                } catch {
-                    print("unexpected error")
-                }
-            }
+            .zIndex(1)
+            
+            // Drawer View
+            DrawerView(isDrawerOpen: $isDrawerOpen, selectedView: $selectedView)
+                .offset(x: isDrawerOpen ? 0 : -UIScreen.main.bounds.width)
         }
-        .scrollIndicators(.hidden)
     }
 }
 
-class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-
-    private let locationManager = CLLocationManager()
-    @Published var locationStatus: CLAuthorizationStatus?
-    @Published var lastLocation: CLLocation?
-
-    override init() {
-        super.init()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-    }
-
-   
+struct DrawerView: View {
+    @Binding var isDrawerOpen: Bool
+    @Binding var selectedView: String
     
-    var statusString: String {
-        guard let status = locationStatus else {
-            return "unknown"
-        }
-        
-        switch status {
-        case .notDetermined: return "notDetermined"
-        case .authorizedWhenInUse: return "authorizedWhenInUse"
-        case .authorizedAlways: return "authorizedAlways"
-        case .restricted: return "restricted"
-        case .denied: return "denied"
-        default: return "unknown"
-        }
-    }
-
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        locationStatus = status
-        print(#function, statusString)
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        lastLocation = location
-        print(#function, location)
-    }
-    
-    func requestPermission() {
-        switch locationStatus {
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        case .denied, .restricted:
-            print("Location services were previously denied. Please enable location services for this app in settings.")
-        case .authorizedWhenInUse, .authorizedAlways, .none:
-            break
-        @unknown default:
-            print("Unknown location services authorization status")
+    var body: some View {
+        ZStack {
+            Color.blue
+            VStack {
+                Button(action: {
+                    withAnimation {
+                        self.isDrawerOpen = false
+                    }
+                }) {
+                    Text("Close")
+                }
+                Button(action: {
+                    withAnimation {
+                        self.isDrawerOpen = false
+                        self.selectedView = "Home"
+                    }
+                }) {
+                    Text("Home")
+                }
+                Button(action: {
+                    withAnimation {
+                        self.isDrawerOpen = false
+                        self.selectedView = "Screen One"
+                    }
+                }) {
+                    Text("Stops Nearby")
+                }
+                Button(action: {
+                    withAnimation {
+                        self.isDrawerOpen = false
+                        self.selectedView = "Screen Two"
+                    }
+                }) {
+                    Text("Screen Two")
+                }
+                Spacer()
+            }
+            .padding()
+            .foregroundColor(.white)
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
         }
     }
 }
